6e3f04326114f889c30d9df7d73cb16c79c1b6be
evgeniy novakovskiy <allidon93@gmail.com>
2022-10-16 20:07:59 +0700

    Finished top and bottom parts

--- src/components/Main/BottomPart/BottomPart.jsx
+++ src/components/Main/BottomPart/BottomPart.jsx
@@ -1,9 +1,43 @@
 import style from "./bottompart.module.scss";
+import ginger from "../../../images/ginger.png";
+import nootris from "../../../images/nootris.png";
+import virus from "../../../images/virus.png";
 
 const BottomPart = () => {
   return (
-    <section>BottomPart</section>
+    <section className={style.bottom}>
+      <div className={style.bottom_content}>
+        <h2 className={style.bottom_heading}>
+          ПОЛУЧИ ЗАЩИТУ ИММУНИТЕТА <br />
+          <span className={style.discount}>СО СКИДКОЙ -15% ПЕРВЫМ!</span>
+        </h2>
+        <h3 className={style.price}>
+          <span className={style.price_old}>750₽</span>690₽
+        </h3>
+        <div className={style.features}>
+          <div className={style.features_item}>
+            <img src={ginger} alt="ginger photo" />
+            <p className={style.features_item_description}>
+              Содержит <br /> <span>имбирь</span>
+            </p>
+          </div>
+          <div className={style.features_special_item}>
+            <img src={nootris} alt="nootris photo" />
+            <p className={style.features_item_description}>
+              + Бесплатная доставка <br /> <span>Специальная цена</span>
+            </p>
+          </div>
+          <div className={style.features_item}>
+            <img src={virus} alt="virus photo" />
+            <p className={style.features_item_description}>
+              Нейтрализует <br /> <span>вирусы</span>
+            </p>
+          </div>
+        </div>
+        <button className={style.button}>Оформить заказ!</button>
+      </div>
+    </section>
   );
-}
+};
 
 export default BottomPart;
--- src/components/Main/TopPart/TopPart.jsx
+++ src/components/Main/TopPart/TopPart.jsx
@@ -2,8 +2,20 @@ import style from "./toppart.module.scss";
 
 const TopPart = () => {
   return (
-    <section>TopPart</section>
+    <section className={style.top}>
+      <div className={style.top_content}>
+        <h1 className={style.main_heading}>Активируй иммунитет!</h1>
+        <p className={style.main_description}>
+          Всего пять секунд в день помогут укрепить иммунитет и повысить
+          защитные силы организма
+        </p>
+        <h3 className={style.secondary_heading}>NOOTRIS ПОМОГАЕТ</h3>
+        <p className={style.secondary_description}>
+          Вашему организму во время пандемии и сезонных простуд
+        </p>
+      </div>
+    </section>
   );
-}
+};
 
 export default TopPart;
